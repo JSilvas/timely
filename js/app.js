1224c6113c2013a5f5ecf10d06e9dcaa0e1dd624
Judah Hunger <judah.hunger@gmail.com>
2018-02-26 16:09:23 -0800

    added eventlistener to login and store to LS need to reset form and add target window.

--- js/app.js
+++ js/app.js
@@ -1,16 +1,61 @@
 'use strict';
-
+//GLOBAL VARS
+var loginForm = document.getElementById('login-form');
 //create an array to hold all users
 User.allUsers = [];
 //create variable to hold current user
 User.currentUser = {name:'', password:''};
 //create user constructor
+checkStoredUsers();
 function User(username, password){
   this.username = username;
   this.password = password;
   User.allUsers.push(this);
 }
+loginForm.addEventListener('submit', loginHandler);
 
+function saveUserToLocal(){
+  localStorage.setItem('allUsers', JSON.stringify(User.allUsers));
+}
+
+function saveCurrentUser() {
+  localStorage.setItem('currentUser', JSON.stringify(User.currentUser));
+}
+
+function loginHandler(event) {
+  event.preventDefault();
+  var name = event.target.user.value;
+  var password = event.target.password.value;
+  User.currentUser.name = name.toLowerCase();
+  //check if current user is in array of all users
+  for(var i = 0; i < User.allUsers.length; i++){
+    if(User.allUsers[i].username === name.toLowerCase() && User.allUsers[i].password === password){
+      User.currentUser = User.allUsers[i];
+      saveCurrentUser();
+      break;
+    }
+  }
+  if( i === User.allUsers.length){
+    new User(name.toLowerCase(), password);
+    event.target.reset();
+    saveUserToLocal();
+    saveCurrentUser();
+  }
+}
+function checkStoredUsers(){
+  if(localStorage.allUsers){
+    var users = JSON.parse(localStorage.getItem('allUsers'));
+    for(var i = 0; i < users.length; i ++){
+      User.allUsers.push(users[i]);
+    }
+  }
+  if(localStorage.currentUser){
+    var currentUser = JSON.parse(localStorage.getItem('currentUser'));
+    User.currentUser = currentUser;
+  }
+}
+
+//********************* */
 //Clock function to keep track of time with date function.
 function startClock(){
   var today = new Date();
@@ -27,3 +72,4 @@ function checkTime(i) {
   return i;
 }
 startClock();
+//*********************** */
