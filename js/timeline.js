749f206618553fa6e7343d7708e21933b4ea049e
Judah Hunger <judah.hunger@gmail.com>
2018-03-01 20:22:50 -0800

    re-added user msg on timeline page

--- js/timeline.js
+++ js/timeline.js
@@ -331,3 +331,19 @@ function logoutHandler(event) {
 }
 
 logoutButton.addEventListener('click', logoutHandler);
+
+
+function welcomeUser(){
+  var myDate = new Date();
+  var hrs = myDate.getHours();
+  var greet;
+  if (hrs < 12)
+    greet = 'Good Morning';
+  else if (hrs >= 12 && hrs <= 17)
+    greet = 'Good Afternoon';
+  else if (hrs >= 17 && hrs <= 24)
+    greet = 'Good Evening';
+
+  document.getElementById('greetings').innerHTML = greet + ' ' + currentUser.charAt(0).toUpperCase() + currentUser.slice(1) + ' and welcome Timely';
+}
+welcomeUser(); 
